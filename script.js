d588752225d12562ea8f9bb42385e09cbb3c471a
Saniya Shete <shetesaniya22@gmail.com>
2026-01-09 14:11:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function getFormvalue() {
-    //Write your code here
-	event.preventDefault();
-	let fname=document.querySelector('input[name="fname"]')
-	let lname=document.querySelector('input[name="lname"]')
-	
-	if(fname="" && laname=""){
-		alert("Please enter your name")
-		return;
-	}
-	alert(fname+" "+lname);
+function getFormvalue(event) {
+    event.preventDefault(); // stop page reload
 
+    let fname = document.querySelector('input[name="fname"]').value.trim();
+    let lname = document.querySelector('input[name="lname"]').value.trim();
 
+    if (fname === "" && lname === "") {
+        alert("Please enter your name");
+        return;
+    }
+
+    alert(fname + " " + lname);
 }
